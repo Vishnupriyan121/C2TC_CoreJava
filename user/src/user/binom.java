4ce26018299c3acbb07c85b5c22c1cadd2c7304e
Hp <Hp@LAPTOP-DE0GJHIK>
2023-07-28 10:12:08 +0530

    vishnupriyyyan

---
+++ user/src/user/binom.java
@@ -0,0 +1,14 @@
+package user;
+
+public class binom {
+
+	public binom() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
